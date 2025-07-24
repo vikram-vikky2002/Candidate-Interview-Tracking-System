ce16428b0b6a02d3e12cdf07ecd1753f2785cbdd
PG <Pragadeswaranofficial@gmail.com>
2025-07-24 22:43:11 +0530

    Utility Done

---
+++ CITS_solution/CITS_DataAccessLayer/DTOs/CandidateProgressDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITS_DataAccessLayer.DTOs
+{
+    public class CandidateProgressDto
+    {
+        public string CandidateName { get; set; }
+        public string CurrentStage { get; set; }
+        public List<string> CompletedStages { get; set; }
+    }
+}
