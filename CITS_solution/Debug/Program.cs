8f0ae974df8050818a1758c30569201dd0d07066
VIKRAM K R <97469502+vikram-vikky2002@users.noreply.github.com>
2025-07-25 14:35:20 +0530

    merge conflict

---
+++ CITS_solution/Debug/Program.cs
@@ -0,0 +1,15 @@
+﻿using CITS_DataAccessLayer;
+
+namespace Debug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InterviewRepository repository = new InterviewRepository();
+
+            var res = repository.GetInterviewsByInterviewer(1);
+            Console.WriteLine(res.Count);
+        }
+    }
+}
