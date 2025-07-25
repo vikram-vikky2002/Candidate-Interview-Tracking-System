30beea510dc190c864223080e66914097e7d5d6c
Narendar <narendar.r@nexturn.com>
2025-07-25 14:13:18 +0530

    API_User

--- CITS_solution/CITS_DataAccessLayer/obj/Debug/net8.0/CITS_DataAccessLayer.AssemblyInfo.cs
+++ CITS_solution/CITS_DataAccessLayer/obj/Debug/net8.0/CITS_DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CITS_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a3aed04897b367b431e554b19983ad834566aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a090fe3a6af40de19c1ef100f157c1c68c71701e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CITS_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CITS_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
