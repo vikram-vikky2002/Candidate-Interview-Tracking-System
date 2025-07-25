bb6cd78ee2b5c37c1661047152702be1462e0044
VIKRAM K R <97469502+vikram-vikky2002@users.noreply.github.com>
2025-07-25 22:20:31 +0530

    bug fixed : working backend

--- CITS_solution/CITS_WebServices/Program.cs
+++ CITS_solution/CITS_WebServices/Program.cs
@@ -14,7 +14,12 @@ namespace CITS_WebServices
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(
+                c =>
+                {
+                    c.CustomSchemaIds(type => type.FullName);
+                }
+            );
 
             builder.Services.AddTransient<InterviewRepository>();
 
