ce43e20d01353caed5a695706540f74ee594eeb8
PG <Pragadeswaranofficial@gmail.com>
2025-07-25 12:17:58 +0530

    pull finish

--- CITS_solution/CITS_DataAccessLayer/obj/Debug/net8.0/CITS_DataAccessLayer.AssemblyInfo.cs
+++ CITS_solution/CITS_DataAccessLayer/obj/Debug/net8.0/CITS_DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CITS_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a787400be1fd6cc14f2515966e265408ef36f52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5667054fa89b052f55360a44e7f4baad91213fa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CITS_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CITS_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
