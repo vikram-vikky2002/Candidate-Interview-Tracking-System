d0a3aed04897b367b431e554b19983ad834566aa
PG <Pragadeswaranofficial@gmail.com>
2025-07-25 13:45:50 +0530

    merging ESU

--- CITS_solution/CITS_DataAccessLayer/obj/Debug/net8.0/CITS_DataAccessLayer.AssemblyInfo.cs
+++ CITS_solution/CITS_DataAccessLayer/obj/Debug/net8.0/CITS_DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CITS_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5667054fa89b052f55360a44e7f4baad91213fa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0bbe4b447e223f662abd4eff860b637d8a6ffe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CITS_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CITS_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
