c5d0be5099eb0ee35f04530b8bc0db5e4f1e547c
Narendar <narendar.r@nexturn.com>
2025-07-27 11:15:41 +0530

    Merged

--- CITS_solution/EUS/obj/Debug/net8.0/CITS_ConsoleApp.AssemblyInfo.cs
+++ CITS_solution/EUS/obj/Debug/net8.0/CITS_ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CITS_ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc5698f9e5fbb3ebcfba893997d5c3de7c244c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91f1b14eb7bab25f38ddd213ee9060e4726e7b0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CITS_ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CITS_ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
